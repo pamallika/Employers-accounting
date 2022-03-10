d1bb35243cb7590722482dc03eccf561dc907c5e
pamallika <sasha.fedorenko4736@gmail.com>
2022-03-10 21:58:29 +0300

    first-commit

---
+++ src/components/app/app.js
@@ -0,0 +1,31 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import './app.css';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
+function App(){
+    const data=[
+        {name:'John C.' , salary:800, increase:false, id:1 },
+        {name:'Alex M.' , salary:3000, increase:true, id:2 },
+        {name:'Carl W.' , salary:15000, increase:false, id:3 }
+    ];
+    return (
+        <div className="app">
+            <AppInfo/>
+
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+                <EmployersList data={data}/>
+                <EmployersAddForm/>
+        </div>
+    );
+}
+
+
+
+export default App;
